21298551c7bdc63a1ffe628f9c0c5b07d980666d
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2024-05-16 18:08:19 +0200

    Raname all occurences from myos to barebones

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/barebones.kernel isodir/boot/barebones.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "barebones" {
+	multiboot /boot/barebones.kernel
 }
 EOF
-grub2-mkrescue -o myos.iso isodir
+grub2-mkrescue -o barebones.iso isodir
